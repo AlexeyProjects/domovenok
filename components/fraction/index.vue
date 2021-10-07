2e620261cb4ce60bd4f8d70f3e53f2e93b73d4a4
AlexeyProjects <62065851+AlexeyProjects@users.noreply.github.com>
2021-10-07 07:52:48 +0400

    `
    `

---
+++ components/fraction/index.vue
@@ -0,0 +1,73 @@
+<template>
+	<div class="fraction">
+		<div class="fraction-item__delete">
+			
+		</div>
+		<input 
+		 	class="fraction-item denominator"
+		    maxlength="2" 
+		    type="text" 
+		    v-model="numeratorVal"
+		    oninput="this.value = this.value.replace(/[^0-9]/g, '')"
+		>
+		<input 
+		 	class="fraction-item denominator"
+		    maxlength="2" 
+		    type="text" 
+		    v-model="denominatorVal"
+		    oninput="this.value = this.value.replace(/[^0-9]/g, '')"
+		>
+	</div>
+</template>
+
+<script>
+	export default {
+		watch: {
+			numeratorVal(val) {
+				this.numeratorVal = +val
+			},
+			denominatorVal(val) {
+				this.denominatorVal = +val
+			}
+		},
+		props: {
+			numerator: 0,
+			denominator: 0
+		},
+		data() {
+			return {
+				numeratorVal: this.numerator,
+				denominatorVal: this.denominator
+			}
+		},
+
+	}
+</script>
+
+<style >
+	html {
+		
+	}
+	.fraction {
+		display: inline-flex;
+		flex-direction: column;
+
+	}
+
+	.fraction-item {
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 4rem;
+		height: 4rem;
+	}
+
+	.fraction-item.numerator {
+		border-bottom: 1px solid #000000;
+		padding-bottom: 1rem;
+	}
+
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="">
+    <Fraction
+    :numerator='numerator'
+    :denominator="denominator"
+    ></Fraction>
+
+  </div>
+</template>
+
+<script>
+export default {
+  watch: {
+
+  },
+  data() {
+    return {
+      numerator: 10,
+      denominator: 100
+    }
+  },
+  methods: {
+    convertNumber(val) {
+      console.log($event.target.value)
+    }
+  },
+  mounted() {
+    console.log(+'00')
+  }
+}
+</script>
+
+<style>
+  html {
+    font-size: 10px;
+  }
+  body {
+    font-size: 1.8rem;
+    font-family: 'Open Sans',sans-serif;
+    padding: 2rem 10rem;
+  }
+  * {
+    box-sizing: border-box;
+    padding:  0;
+    margin: 0;
+  }
+  .row {
+    display: flex;  
+  }
+</style>
